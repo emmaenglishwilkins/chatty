291381bfbec1ec1810ad090720b5697cc751de5c
emmaenglishwilkins <emmawilkins@penguincodingschool.com>
2025-03-31 17:31:53 -0400

    added basic chatbot -  "attention mask is not set and cannot be inferred "

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session
 from chatbot import chatbot_response 
+from chatbot import ai_response
 
 app = Flask(__name__)
 app.secret_key = 'penguin'
@@ -7,15 +8,15 @@ app.secret_key = 'penguin'
 @app.route("/")
 def home():
     session['state'] = 0
-	return render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/", methods=['POST'])
 def botresponse():
     userInput = request.form['msg']
     state = session['state']
-    print(state)
-    print(chatbot_response(state, userInput))
-    state, chatResponse = chatbot_response(state, userInput)
+
+    state, chatResponse = ai_response(state, userInput)
+
     session['state'] = state
     return render_template("index.html", data=userInput,data2=chatResponse)
 
--- chatbot.py
+++ chatbot.py
@@ -3,13 +3,29 @@ import re
 import random
 from textblob import TextBlob, Word
 
+# DB set up 
 conn = sqlite3.connect('chatty1.db', check_same_thread=False)
 c = conn.cursor()
 
-'''
-#everything else 
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-'''
+model_name = "gpt2"
+model = GPT2LMHeadModel.from_pretrained(model_name)
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+model.eval()
+
+def ai_response(state, userInput): 
+    inputs = tokenizer.encode(userInput + tokenizer.eos_token, return_tensors="pt")
+    response = model.generate(
+        inputs, 
+        max_length=100, 
+        num_return_sequences=1, 
+        pad_token_id=tokenizer.eos_token_id
+    )
+    chat_response = tokenizer.decode(response[0], skip_special_tokens=True)
+    state += 1
+    return state, chat_response
 
 def chatbot_response(state, userInput):
     flag = True
